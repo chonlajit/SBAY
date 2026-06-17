a7ee28b1582e800d296340f56e06052dabf9acff
Chonlasit <chonlasit.ji@rmuti.ac.th>
2026-06-17 19:36:47 +0700

    Keep only iot-device folder

--- backend/src/main/java/com/example/iotbackend/repository/RewardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.iotbackend.repository;
-
-import com.example.iotbackend.model.Reward;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RewardRepository extends MongoRepository<Reward, String> {
-}
