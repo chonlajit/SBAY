ec2f6742c6279fcbad101a7e233ebf3bb0ee29e0
Chonlasit <chonlasit.ji@rmuti.ac.th>
2026-09-01 20:20:50 +0700

    Partner redict

--- frontend/app/partner/page.tsx
+++ frontend/app/partner/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
-import { useSmartBin } from '../../context/SmartBinContext';
+import { useSmartBin } from '../context/SmartBinContext';
 
 interface PartnerReward {
     id: string;
